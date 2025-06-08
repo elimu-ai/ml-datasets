930c8bcc903f41358bdf4787f5dcce1289e426ad
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-06-08 17:50:08 +0700

    refactor: categorize datasets by student ids
    #8

--- download_datasets.py
+++ download_datasets.py
@@ -18,36 +18,68 @@ for language in languages:
 
     os.makedirs(f'lang-{language}', exist_ok=True)
 
-    letter_sound_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/letter-sound-learning-event/list/letter-sound-learning-events.csv'
-    print(f'letter_sound_learning_events_csv_url: {letter_sound_learning_events_csv_url}')
-    letter_sound_learning_events_dataframe = pandas.read_csv(letter_sound_learning_events_csv_url)
-    print(f'letter_sound_learning_events_dataframe: \n{letter_sound_learning_events_dataframe}')
-    letter_sound_learning_events_file_path = f'lang-{language}/letter_sound-learning-events.csv'
-    print(f'letter_sound_learning_events_file_path: {letter_sound_learning_events_file_path}')
-    letter_sound_learning_events_dataframe.to_csv(letter_sound_learning_events_file_path, index=False)
+    students_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/students.csv'
+    print(f'students_csv_url: {students_csv_url}')
+    students_dataframe = pandas.read_csv(students_csv_url)
+    print(f'students_dataframe: \n{students_dataframe}')
+    students_file_path = f'lang-{language}/students.csv'
+    print(f'students_file_path: {students_file_path}')
+    students_dataframe.to_csv(students_file_path, index=False)
 
-    word_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/word-learning-event/list/word-learning-events.csv'
-    print(f'word_learning_events_csv_url: {word_learning_events_csv_url}')
-    word_learning_events_dataframe = pandas.read_csv(word_learning_events_csv_url)
-    print(f'word_learning_events_dataframe: \n{word_learning_events_dataframe}')
-    word_learning_events_file_path = f'lang-{language}/word-learning-events.csv'
-    print(f'word_learning_events_file_path: {word_learning_events_file_path}')
-    word_learning_events_dataframe.to_csv(word_learning_events_file_path, index=False)
+    for index, student in students_dataframe.iterrows():
+        print()
+        print(f'student.id: {student.id}')
 
-    # TODO: number learning events
-    
-    storybook_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/storybook-learning-event/list/storybook-learning-events.csv'
-    print(f'storybook_learning_events_csv_url: {storybook_learning_events_csv_url}')
-    storybook_learning_events_dataframe = pandas.read_csv(storybook_learning_events_csv_url)
-    print(f'storybook_learning_events_dataframe: \n{storybook_learning_events_dataframe}')
-    storybook_learning_events_file_path = f'lang-{language}/storybook-learning-events.csv'
-    print(f'storybook_learning_events_file_path: {storybook_learning_events_file_path}')
-    storybook_learning_events_dataframe.to_csv(storybook_learning_events_file_path, index=False)
+        os.makedirs(f'lang-{language}/student-id-{student.id}', exist_ok=True)
 
-    video_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/video-learning-event/list/video-learning-events.csv'
-    print(f'video_learning_events_csv_url: {video_learning_events_csv_url}')
-    video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
-    print(f'video_learning_events_dataframe: \n{video_learning_events_dataframe}')
-    video_learning_events_file_path = f'lang-{language}/video-learning-events.csv'
-    print(f'video_learning_events_file_path: {video_learning_events_file_path}')
-    video_learning_events_dataframe.to_csv(video_learning_events_file_path, index=False)
+        letter_sound_assessment_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/letter-sound-assessment-events.csv'
+        print(f'letter_sound_assessment_events_csv_url: {letter_sound_assessment_events_csv_url}')
+        letter_sound_assessment_events_dataframe = pandas.read_csv(letter_sound_assessment_events_csv_url)
+        print(f'letter_sound_assessment_events_dataframe: \n{letter_sound_assessment_events_dataframe}')
+        letter_sound_assessment_events_file_path = f'lang-{language}/student-id-{student.id}/letter_sound-assessment-events.csv'
+        print(f'letter_sound_assessment_events_file_path: {letter_sound_assessment_events_file_path}')
+        letter_sound_assessment_events_dataframe.to_csv(letter_sound_assessment_events_file_path, index=False)
+        
+        letter_sound_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/letter-sound-learning-events.csv'
+        print(f'letter_sound_learning_events_csv_url: {letter_sound_learning_events_csv_url}')
+        letter_sound_learning_events_dataframe = pandas.read_csv(letter_sound_learning_events_csv_url)
+        print(f'letter_sound_learning_events_dataframe: \n{letter_sound_learning_events_dataframe}')
+        letter_sound_learning_events_file_path = f'lang-{language}/student-id-{student.id}/letter_sound-learning-events.csv'
+        print(f'letter_sound_learning_events_file_path: {letter_sound_learning_events_file_path}')
+        letter_sound_learning_events_dataframe.to_csv(letter_sound_learning_events_file_path, index=False)
+
+        word_assessment_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/word-assessment-events.csv'
+        print(f'word_assessment_events_csv_url: {word_assessment_events_csv_url}')
+        word_assessment_events_dataframe = pandas.read_csv(word_assessment_events_csv_url)
+        print(f'word_assessment_events_dataframe: \n{word_assessment_events_dataframe}')
+        word_assessment_events_file_path = f'lang-{language}/student-id-{student.id}/word-assessment-events.csv'
+        print(f'word_assessment_events_file_path: {word_assessment_events_file_path}')
+        word_assessment_events_dataframe.to_csv(word_assessment_events_file_path, index=False)
+        
+        word_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/word-learning-events.csv'
+        print(f'word_learning_events_csv_url: {word_learning_events_csv_url}')
+        word_learning_events_dataframe = pandas.read_csv(word_learning_events_csv_url)
+        print(f'word_learning_events_dataframe: \n{word_learning_events_dataframe}')
+        word_learning_events_file_path = f'lang-{language}/student-id-{student.id}/word-learning-events.csv'
+        print(f'word_learning_events_file_path: {word_learning_events_file_path}')
+        word_learning_events_dataframe.to_csv(word_learning_events_file_path, index=False)
+
+        # TODO: number assessment events
+        
+        # TODO: number learning events
+        
+        storybook_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/storybook-learning-events.csv'
+        print(f'storybook_learning_events_csv_url: {storybook_learning_events_csv_url}')
+        storybook_learning_events_dataframe = pandas.read_csv(storybook_learning_events_csv_url)
+        print(f'storybook_learning_events_dataframe: \n{storybook_learning_events_dataframe}')
+        storybook_learning_events_file_path = f'lang-{language}/student-id-{student.id}/storybook-learning-events.csv'
+        print(f'storybook_learning_events_file_path: {storybook_learning_events_file_path}')
+        storybook_learning_events_dataframe.to_csv(storybook_learning_events_file_path, index=False)
+
+        video_learning_events_csv_url = f'http://{language.lower()}.elimu.ai/analytics/students/{student.id}/video-learning-events.csv'
+        print(f'video_learning_events_csv_url: {video_learning_events_csv_url}')
+        video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+        print(f'video_learning_events_dataframe: \n{video_learning_events_dataframe}')
+        video_learning_events_file_path = f'lang-{language}/student-id-{student.id}/video-learning-events.csv'
+        print(f'video_learning_events_file_path: {video_learning_events_file_path}')
+        video_learning_events_dataframe.to_csv(video_learning_events_file_path, index=False)
